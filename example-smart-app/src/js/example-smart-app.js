ff4867460551973c2bff23a97726a8dab83cd4e5
Muhammad Meran Farooq <163828755+mmeranfarooq@users.noreply.github.com>
2025-04-30 16:25:01 +0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -7,7 +7,7 @@
       ret.reject();
     }
 
- /*  function onReady(smart)  {
+ function onReady(smart)  {
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
@@ -25,6 +25,33 @@
         $.when(pt, obv).fail(onError);
 
         $.when(pt, obv).done(function(patient, obv) {
+
+           console.log("SMART FHIR Client Object:", smart);
+
+          // Server info
+          if (smart.server) {
+            console.log("FHIR Server Info:", smart.server);
+          }
+        
+          // Patient access
+          if (smart.patient) {
+            console.log("Patient object:", smart.patient);
+          }
+        
+          // API object for querying
+          if (smart.patient && smart.patient.api) {
+            console.log("SMART API object (query interface):", smart.patient.api);
+          }
+        
+          // Server capability statement (may be a Promise)
+          if (smart.server && smart.server.metadata) {
+            smart.server.metadata.then(function (capabilities) {
+              console.log("FHIR Server Capabilities (metadata):", capabilities);
+            }).catch(function (err) {
+              console.error("Error loading metadata:", err);
+            });
+          }
+                  
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
@@ -65,136 +92,7 @@
       } else {
         onError();
       }
-    }*/
-
- function onReady(smart) {
-  if (smart.hasOwnProperty('patient')) {
-    var patient = smart.patient;
-    var pt = patient.read();
-
-    var obv = smart.patient.api.fetchAll({
-      type: 'Observation',
-      query: {
-        code: {
-          $or: [
-            'http://loinc.org|8302-2',
-            'http://loinc.org|8462-4',
-            'http://loinc.org|8480-6',
-            'http://loinc.org|2085-9',
-            'http://loinc.org|2089-1',
-            'http://loinc.org|55284-4'
-          ]
-        }
-      }
-    });
-
-    // Default resource fetches
-    const fetches = {
-      allergies: 'AllergyIntolerance',
-      conditions: 'Condition',
-      medications: 'MedicationRequest',
-      immunizations: 'Immunization',
-      procedures: 'Procedure',
-      encounters: 'Encounter',
-      carePlans: 'CarePlan',
-      reports: 'DiagnosticReport',
-      documents: 'DocumentReference'
-    };
-
-    const resourcePromises = {};
-    for (const [key, resourceType] of Object.entries(fetches)) {
-      if (smart.patient.api.conformance.resources.some(r => r.type === resourceType)) {
-        resourcePromises[key] = smart.patient.api.fetchAll({ type: resourceType });
-      } else {
-        console.log(`${resourceType} not supported by server.`);
-        resourcePromises[key] = $.Deferred().resolve([]).promise(); // dummy empty
-      }
     }
-
-    $.when(
-      pt,
-      obv,
-      resourcePromises.allergies,
-      resourcePromises.conditions,
-      resourcePromises.medications,
-      resourcePromises.immunizations,
-      resourcePromises.procedures,
-      resourcePromises.encounters,
-      resourcePromises.carePlans,
-      resourcePromises.reports,
-      resourcePromises.documents
-    ).done(function (
-      patient,
-      obv,
-      allergies,
-      conditions,
-      medications,
-      immunizations,
-      procedures,
-      encounters,
-      carePlans,
-      reports,
-      documents
-    ) {
-      try {
-        var byCodes = smart.byCodes(obv, 'code');
-        var gender = patient.gender;
-        var fname = '';
-        var lname = '';
-
-        if (typeof patient.name[0] !== 'undefined') {
-          fname = patient.name[0].given.join(' ');
-          lname = patient.name[0].family.join(' ');
-        }
-
-        var height = byCodes('8302-2');
-        var systolicbp = getBloodPressureValue(byCodes('55284-4'), '8480-6');
-        var diastolicbp = getBloodPressureValue(byCodes('55284-4'), '8462-4');
-        var hdl = byCodes('2085-9');
-        var ldl = byCodes('2089-1');
-
-        var p = defaultPatient();
-        p.birthdate = patient.birthDate;
-        p.gender = gender;
-        p.fname = fname;
-        p.lname = lname;
-        p.height = getQuantityValueAndUnit(height[0]);
-
-        if (systolicbp) p.systolicbp = systolicbp;
-        if (diastolicbp) p.diastolicbp = diastolicbp;
-        if (hdl[0]) p.hdl = getQuantityValueAndUnit(hdl[0]);
-        if (ldl[0]) p.ldl = getQuantityValueAndUnit(ldl[0]);
-
-        // Logging available resources
-        if (allergies.length) console.log("Allergies:", allergies);
-        if (conditions.length) console.log("Conditions:", conditions);
-        if (medications.length) console.log("Medications:", medications);
-        if (immunizations.length) console.log("Immunizations:", immunizations);
-        if (procedures.length) console.log("Procedures:", procedures);
-        if (encounters.length) console.log("Encounters:", encounters);
-        if (carePlans.length) console.log("Care Plans:", carePlans);
-        if (reports.length) console.log("Diagnostic Reports:", reports);
-        if (documents.length) console.log("Document References:", documents);
-
-        // Optional UI display (keeping previous functionality)
-        $('body').append('<h3>Allergies:</h3><pre>' + JSON.stringify(allergies, null, 2) + '</pre>');
-        $('body').append('<h3>Conditions:</h3><pre>' + JSON.stringify(conditions, null, 2) + '</pre>');
-        $('body').append('<h3>Medications:</h3><pre>' + JSON.stringify(medications, null, 2) + '</pre>');
-        $('body').append('<h3>Immunizations:</h3><pre>' + JSON.stringify(immunizations, null, 2) + '</pre>');
-
-        ret.resolve(p);
-      } catch (e) {
-        console.error("Error processing patient data:", e);
-      }
-    }).fail(function () {
-      console.error("One or more fetches failed.");
-      onError();
-    });
-  } else {
-    onError();
-  }
-}
-
     FHIR.oauth2.ready(onReady, onError);
     return ret.promise();
 
