a63c9da3f08bb10639fc766f5579be30221028e9
Muhammad Meran Farooq <163828755+mmeranfarooq@users.noreply.github.com>
2025-04-30 15:45:45 +0400

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -67,156 +67,115 @@
       }
     }*/
     function onReady(smart) {
-      if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
-        var pt = patient.read();
-    
-        var obv = smart.patient.api.fetchAll({
-          type: 'Observation',
-          query: {
-            code: {
-              $or: [
-                'http://loinc.org|8302-2', // height
-                'http://loinc.org|8462-4', // diastolic
-                'http://loinc.org|8480-6', // systolic
-                'http://loinc.org|2085-9', // HDL
-                'http://loinc.org|2089-1', // LDL
-                'http://loinc.org|55284-4' // Blood pressure panel
-              ]
-            }
-          }
-        });
-    
-        // Additional FHIR resources
-        var allergies = smart.patient.api.fetchAll({ type: 'AllergyIntolerance' });
-        var conditions = smart.patient.api.fetchAll({ type: 'Condition' });
-        var medications = smart.patient.api.fetchAll({ type: 'MedicationRequest' });
-     //   var immunizations = smart.patient.api.fetchAll({ type: 'Immunization' });
-    
-        $.when(pt, obv, allergies, conditions, medications).fail(onError);
-    
-        $.when(pt, obv, allergies, conditions, medications).done(function (
-          patient,
-          obv,
-          allergies,
-          conditions,
-          medications
-        ) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
-          var fname = '';
-          var lname = '';
-    
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
-          }
-    
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'), '8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'), '8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
-    
-          var p = defaultPatient();
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
-    
-          if (typeof systolicbp !== 'undefined') {
-            p.systolicbp = systolicbp;
-          }
-    
-          if (typeof diastolicbp !== 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
-    
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
-    
-          // Print additional resources to body
-          $('body').append('<h3>Allergies:</h3><pre>' + JSON.stringify(allergies, null, 2) + '</pre>');
-          $('body').append('<h3>Conditions:</h3><pre>' + JSON.stringify(conditions, null, 2) + '</pre>');
-          $('body').append('<h3>Medications:</h3><pre>' + JSON.stringify(medications, null, 2) + '</pre>');
-        //  $('body').append('<h3>Immunizations:</h3><pre>' + JSON.stringify(immunizations, null, 2) + '</pre>');
-    
-          ret.resolve(p);
-        });
-      } else {
-        onError();
+  if (smart.hasOwnProperty('patient')) {
+    var patient = smart.patient;
+    var pt = patient.read();
+
+    var obv = smart.patient.api.fetchAll({
+      type: 'Observation',
+      query: {
+        code: {
+          $or: [
+            'http://loinc.org|8302-2',
+            'http://loinc.org|8462-4',
+            'http://loinc.org|8480-6',
+            'http://loinc.org|2085-9',
+            'http://loinc.org|2089-1',
+            'http://loinc.org|55284-4'
+          ]
+        }
       }
+    });
+
+    // Safe fetches with error catching
+    var allergies = smart.patient.api.fetchAll({ type: 'AllergyIntolerance' }).catch(e => {
+      console.error("Error fetching allergies:", e);
+      return [];
+    });
+
+    var conditions = smart.patient.api.fetchAll({ type: 'Condition' }).catch(e => {
+      console.error("Error fetching conditions:", e);
+      return [];
+    });
+
+    var medications = smart.patient.api.fetchAll({ type: 'MedicationRequest' }).catch(e => {
+      console.error("Error fetching medications:", e);
+      return [];
+    });
+
+    var immunizations = smart.patient.api.fetchAll({ type: 'Immunization' }).catch(e => {
+      console.error("Error fetching immunizations:", e);
+      return [];
+    });
+
+    $.when(pt, obv, allergies, conditions, medications, immunizations).fail(onError);
+
+    $.when(pt, obv, allergies, conditions, medications, immunizations).done(function (
+      patient,
+      obv,
+      allergies,
+      conditions,
+      medications,
+      immunizations
+    ) {
+      try {
+        var byCodes = smart.byCodes(obv, 'code');
+        var gender = patient.gender;
+
+        var fname = '';
+        var lname = '';
+
+        if (typeof patient.name[0] !== 'undefined') {
+          fname = patient.name[0].given.join(' ');
+          lname = patient.name[0].family.join(' ');
+        }
+
+        var height = byCodes('8302-2');
+        var systolicbp = getBloodPressureValue(byCodes('55284-4'), '8480-6');
+        var diastolicbp = getBloodPressureValue(byCodes('55284-4'), '8462-4');
+        var hdl = byCodes('2085-9');
+        var ldl = byCodes('2089-1');
+
+        var p = defaultPatient();
+        p.birthdate = patient.birthDate;
+        p.gender = gender;
+        p.fname = fname;
+        p.lname = lname;
+        p.height = getQuantityValueAndUnit(height[0]);
+
+        if (typeof systolicbp !== 'undefined') {
+          p.systolicbp = systolicbp;
+        }
+
+        if (typeof diastolicbp !== 'undefined') {
+          p.diastolicbp = diastolicbp;
+        }
+
+        p.hdl = getQuantityValueAndUnit(hdl[0]);
+        p.ldl = getQuantityValueAndUnit(ldl[0]);
+
+        // Log to console (if available)
+        if (allergies.length) console.log("Allergies:", allergies);
+        if (conditions.length) console.log("Conditions:", conditions);
+        if (medications.length) console.log("Medications:", medications);
+        if (immunizations.length) console.log("Immunizations:", immunizations);
+
+        // Print to body (old functionality preserved)
+        $('body').append('<h3>Allergies:</h3><pre>' + JSON.stringify(allergies, null, 2) + '</pre>');
+        $('body').append('<h3>Conditions:</h3><pre>' + JSON.stringify(conditions, null, 2) + '</pre>');
+        $('body').append('<h3>Medications:</h3><pre>' + JSON.stringify(medications, null, 2) + '</pre>');
+        $('body').append('<h3>Immunizations:</h3><pre>' + JSON.stringify(immunizations, null, 2) + '</pre>');
+
+        ret.resolve(p);
+      } catch (e) {
+        console.error("Error in processing patient data:", e);
+      }
+    });
+  } else {
+    onError();
+  }
 }
 
-    
-   /* function onReady(smart) {
-        if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
-        var pt = patient.read();
-    
-        // Fetch multiple resource types
-        var obv = smart.patient.api.fetchAll({ type: 'Observation' });
-        var conditions = smart.patient.api.fetchAll({ type: 'Condition' });
-        var meds = smart.patient.api.fetchAll({ type: 'MedicationRequest' });
-        var allergies = smart.patient.api.fetchAll({ type: 'AllergyIntolerance' });
-        var encounters = smart.patient.api.fetchAll({ type: 'Encounter' });
-    
-        // Wait for all resources to load
-        $.when(pt, obv, conditions, meds, allergies, encounters).fail(onError);
-    
-        $.when(pt, obv, conditions, meds, allergies, encounters).done(function(patient, obv, conditions, meds, allergies, encounters) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
-    
-          var fname = '';
-          var lname = '';
-    
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
-          }
-    
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'), '8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'), '8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
-    
-          var p = defaultPatient();
-          p.birthdate = patient.birthDate;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.height = getQuantityValueAndUnit(height[0]);
-    
-          if (typeof systolicbp != 'undefined') {
-            p.systolicbp = systolicbp;
-          }
-    
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
-    
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
-    
-          // Add fetched resources
-          p.conditions = conditions;
-          p.medications = meds;
-          p.allergies = allergies;
-          p.encounters = encounters;
-    
-          // Render JSON to the page
-          var jsonOutput = JSON.stringify(p, null, 2); // Pretty-print with 2-space indentation
-          $('body').html('<pre>' + jsonOutput + '</pre>'); // Inject into body as <pre>
-          
-          ret.resolve(p);
-        });
-      } else {
-        onError();
-      }
-}*/
     FHIR.oauth2.ready(onReady, onError);
     return ret.promise();
 
